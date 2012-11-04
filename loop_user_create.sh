dab310170bc5de2ad7aed66dfcda3fe9c0615d40
Paul <paulbosox@yahoo.com>
2012-11-03 21:46:29 -0400

    created log files for each new account that is created. It is dated appropriately as well.

--- loop_user_create.sh
+++ loop_user_create.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# this works as of July 12, 20012
-# created 12 accounts for new trainers
+# this creates accounts for people with email accounts
+ 
 
 # for user in $list
 for email in `cat /Users/paul/shell_scripts/drush_user_create/users_example_file`
@@ -20,7 +20,6 @@ drush @live ucrt $username --mail=$email
 
 # omg it has worked up to this point!!!
 # it's 1:22 on Thursday July 12, 2012
-# Let's see if I can do the rest of it
 
 drush @live ublk $username
 drush @live urol "General Role" $username
@@ -32,9 +31,13 @@ drush @live sqlq "UPDATE users SET DATA = (select data2 from dummytable) WHERE n
 
 drush @live uublk $username
 
+echo $username >> "archive_users_created/user_list_$(date +%Y_%m_%d).txt"
+
 echo " "
 echo " "
 echo " the user account $username was created"
-					
-		
+echo " "
+echo "******************************************** "
+echo " "
+echo " "
 done
